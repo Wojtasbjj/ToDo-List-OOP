57bc9205347fcb6e9a5da911d3a394ee6b736d24
WojtasBjj <wojtekgajda94@gmail.com>
2020-11-02 12:08:13 +0100

    dodanie pliku main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+class AddElement {
+    constructor() {
+        const addLi = function() {
+            const ulPanel = document.querySelector('ul');
+            const liElement = document.createElement('li');
+        }
+    }
+}
