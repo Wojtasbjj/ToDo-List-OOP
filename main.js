0c44da26fe64152f5846fca67343668544f55723
WojtasBjj <wojtekgajda94@gmail.com>
2020-11-02 15:53:08 +0100

    dodanie klasy ItemList i metod addItem oraz deleteItem

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-class AddLiItem {
+ // moge zadeklarować tablice tutaj poza obiektem ? 
+const inputText = document.querySelector('input'); // pobieram input tutaj. Lepiej zrobić to gdzieś indziej ? 
+const items = [];
+
+class ItemList {
     constructor() {
-        const addButton = document.querySelector("button");
-
-        const addItem = function() {
-            console.log('działa')
-
-            addButton.addEventListener("click", addItem);
-        }
+        this.textcontent = inputText.value;
     }
+addLi () {
+    
+    items.push(liItem.textcontent)
+    console.log(this)
+}
+deleteLi () {
+    console.log('usuwa element z tablicy')
+}
 }
 
+const liItem = new ItemList();
+console.log(liItem)
 
 
+
+
+// for (i=0; i<items.length; i++) {
+//     const liElement = document.createElement('li');
+//     liElement.textContent = items[i];
+//     ulElement.appendChild(liElement)
+//     console.log(liElement.textContent)
+// }
 
