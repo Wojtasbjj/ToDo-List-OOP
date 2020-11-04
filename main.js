c3130fef981a1c5abb8e08b1dae1b417d3a7d863
WojtasBjj <wojtekgajda94@gmail.com>
2020-11-04 10:51:21 +0100

    elementy li dostają ID, każdy nowy element id++

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
- // moge zadeklarować tablice tutaj poza obiektem ? 
-const inputText = document.querySelector('input'); // pobieram input tutaj. Lepiej zrobić to gdzieś indziej ? 
-const items = [];
 
 class ItemList {
     constructor() {
-        this.textcontent = inputText.value;
+        this.myItems = [];
+        document.querySelector('button').addEventListener('click', this.addLi.bind(this));
+        this.inputText = document.querySelector('input');
+        this.ulElement = document.getElementById('liList')
+        console.log('constraktor koniec');
     }
 addLi () {
-    
-    items.push(liItem.textcontent)
-    console.log(this)
-}
-deleteLi () {
-    console.log('usuwa element z tablicy')
-}
+    if (this.inputText.value == "") {
+        alert('Wprowadź text')
+        return
+    }
+    this.myItems.push(this.inputText.value);
+    console.log(this.myItems);
+
+    this.liElement = document.createElement('li');
+
+    for (let i=0; i < this.myItems.length; i++) {
+        console.log('uruchomienie petli')
+        this.liElement.textContent = this.myItems[i]
+        this.liElement.setAttribute('id', [i])
+        this.liElement.innerHTML += ` <button type="button" class="btn btn-primary" id="doneBtn">done</button>`
+        this.ulElement.appendChild(this.liElement);
+    }
+    this.inputText.value = ""
 }
 
+deleteLi () {
+    console.log('usuwa element z tablicy');
+}
+}
 const liItem = new ItemList();
-console.log(liItem)
+
 
 
 
